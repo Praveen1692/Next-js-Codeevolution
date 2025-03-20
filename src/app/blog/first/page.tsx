e1fbb6f2705cbcc7e81511cc5d40805f30a98d19
Praveen <praying.sharma@gmail.com>
2025-03-20 11:31:58 +0530

    Add

---
+++ src/app/blog/first/page.tsx
@@ -0,0 +1,8 @@
+export default function First() {
+  return (
+    <div>
+      <h1>This is First Page inside the Blog</h1>
+      <p>This is a paragraph from the First Page inside the Blog</p>
+    </div>
+  );
+}
---
+++ src/app/blog/second/page.tsx
@@ -0,0 +1,7 @@
+export default function Second() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline bg-green-400"> This is second page inside the blog</h1>
+    </div>
+  );
+}
