34830de8a1264c1326eb222c0fe3b614e39d8e38
Praveen <praying.sharma@gmail.com>
2025-03-31 17:23:21 +0530

    Add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
+import Link from "next/link";
+
 export default function Home() {
-  return(
+  return (
     <div>
       <h1>Hello Next js Project</h1>
+      <Link href="/about">About</Link>
+      <br />
+      <Link href="/contact">Contact</Link>
+      <br />
+      <Link href="/">Home Page</Link>
     </div>
-  )
-  
+  );
 }
